ae29b20a336d4c314446f261c6e51f4067114ca4
Rafael <rafael271alves@gmail.com>
2024-02-06 10:13:03 -0300

    changed index message

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json('Hello World!');
+    return response()
+        ->json('API Library Challenge');
 });
