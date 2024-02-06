29fb3eaebcc46e2a565df23eb8082cd4a3098bd8
Rafael <rafael271alves@gmail.com>
2024-02-06 16:45:49 -0300

    added route to user controller

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,9 +1,27 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // GET /
 Route::get('/', function () {
     return response()
         ->json('API Library Challenge');
+});
+
+Route::group([
+    'middleware' => ['auth'],
+], function () {
+    Route::group([
+        'prefix' => 'user',
+        'controller' => UserController::class,
+    ], function () {
+        // GET user/profile-information
+        Route::get('profile-information', 'profileInformation')
+            ->name('user-profile-information.index');
+
+        // GET user/devices
+        Route::get('devices', 'devices')
+            ->name('user-devices.index');
+    });
 });
