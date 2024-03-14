b30ef3ebced3d7869910dac6b0becbe03b1261f7
Rafael <rafael271alves@gmail.com>
2024-03-14 13:46:20 -0300

    added nuxt seo

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxt/ui', '@nuxtjs/eslint-module', '@nuxt/image'],
+    modules: ['@nuxt/ui', '@nuxtjs/eslint-module', '@nuxt/image', '@nuxtjs/seo'],
+    site: {
+        url: 'https://library.devrma.com/',
+        name: 'Library Challenge',
+        description: 'A library simulation, where renting books to customers is simulated.',
+        defaultLocale: 'en',
+    },
 });
